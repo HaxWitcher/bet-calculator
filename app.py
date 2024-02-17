1043044be94d1db782595417ee5adbdfb5d6a5be
HaxWitcher <111240651+HaxWitcher@users.noreply.github.com>
2024-02-17 14:28:52 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def calculate():
+    if request.method == 'POST':
+        losses = float(request.form.get('losses'))
+        odds = float(request.form.get('odds'))
+        profit = float(request.form.get('profit'))
+        stake = (losses + profit) / (odds - 1)
+        return render_template('index.html', stake=stake)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
